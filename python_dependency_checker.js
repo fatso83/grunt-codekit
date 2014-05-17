60f35100ec263bf3e9ae55ad4c3c9d837ba3cbc0
Carl-Erik Kopseng <carlerik@gmail.com>
2014-05-18 00:15:35 +0200

    Fixed issue #3: failing 'npm install' on windows

--- python_dependency_checker.js
+++ python_dependency_checker.js
@@ -1,16 +1,22 @@
-#!/usr/bin/env node
-
 var cp = require('child_process');
 
 var pyCmd = 'python',
-      childProcess = cp.exec(pyCmd + ' -c ""', {}, function() { /* do nothing */ }),
       log = process.stderr.write.bind(process.stderr);
 
-childProcess.on('exit', function(code) {
+childProcess = cp.exec(pyCmd + ' -c ""', {}, 
+   function(err, stdout, stderr) {
+          if(err) { 
+                log("ERROR: Received an error trying to check for a working" +
+                        "Python install. Have you installed Python?\n" +
+                        "Actual error: \"" + err + "\"");
+                process.exit(1);
+          }
+   }
+)
 
+childProcess.on('exit', function(code) {
+  /* This only works in unix environments */
   if(code === 127 ) {
     log('Could not find "' + pyCmd + '" executable. It needs to be in the PATH\n'); 
   } else { childProcess.stderr.on('data', function (d) { log(d); }); }
-
-  process.exit(code);
 });
