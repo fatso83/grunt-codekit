d1c784b10effe8cf92a9cc913124f632a5b08f58
Carl-Erik Kopseng <carlerik@gmail.com>
2014-06-08 02:06:46 +0200

    Added js-building

--- tasks/codekit.js
+++ tasks/codekit.js
@@ -9,58 +9,78 @@
 
 'use strict';
 
-function setCwd(grunt, dir) {
-        grunt.file.setBase(dir);
-        grunt.log.debug("New cwd: " + dir);
-}
+var nonPartials = function (filepath) {
+    // Check for Kit partials and don’t include them in the compile list
+    // (They will be compiled only via imports and can cause errors if there
+    // are variables that they use that are defined in the parent scope.)
+    var basefilepath = path.basename(filepath);
+    if (basefilepath[0] === "_") {
+        grunt.verbose.ok("Encountered partial " + filepath + " — not compiling it directly.");
+        return false;
+    }
+    return true;
+};
 
-module.exports = function(grunt) {
-    var cp = require('child_process'),
-        kit = require('node-kit'),
-        fs = require('fs'),
+
+module.exports = function (grunt) {
+    var kit = require('node-kit'),
         path = require('path');
 
-  grunt.registerMultiTask('codekit', 'Compiles files using the open CodeKit language', function() {
+    grunt.registerMultiTask('codekit', 'Compiles files using the open CodeKit language', function () {
 
-    // Iterate over all specified file groups.
-    this.files.forEach(function(f) {
-      // Concat specified files
+        // Iterate over all specified file groups.
+        this.files.forEach(function (f) {
+            var destination = f.dest;
 
-      var src = f.src.filter(function(filepath) {
-        // Check for Kit partials and don’t include them in the compile list
-        // (They will be compiled only via imports and can cause errors if there
-        // are variables that they use that are defined in the parent scope.)
-        var basefilepath = path.basename(filepath);
-        if (basefilepath[0] === "_") {
-          grunt.verbose.ok("Encountered partial " + filepath + " — not compiling it directly.");
-          return false;
-        }
-               
-        // Warn on and remove invalid source files (if nonull was set).
-        if (!grunt.file.exists(filepath)) {
-          grunt.log.error('Source file "' + filepath + '" not found.');
-          return true;
-        } else {
-          return true;
-        }
-      }).map(function(filepath) {
-        var destination = f.dest,
-          html = kit(filepath);
+            var compileKitFile = function (filepath) {
+                var               html;
 
-        grunt.log.debug("Trying path: " + filepath);
-        html = kit(filepath);
+                grunt.log.debug("Trying path: " + filepath);
+                html = kit(filepath);
 
-        // This should not happen, but test anyway
-        if( html === filepath) {
-          grunt.log.warn('Source file "' + filepath + '" not found.');
-          return;
-        }
+                // This should not happen, but test anyway
+                if (html === filepath) {
+                    grunt.log.warn('Source file "' + filepath + '" not found.');
+                    return;
+                }
 
-        grunt.log.debug("Got html: " + html);
-        grunt.log.debug("Writing file : " + destination);
-        grunt.file.write(destination, html );
-      });
+                grunt.log.debug("Got html: " + html);
+                grunt.log.debug("Writing file : " + destination);
+                grunt.file.write(destination, html);
+            };
+
+
+            function compileJsFile(filepath) {
+                var builder = require('file-builder')
+                    , fileOptions = {
+                        input: filepath,
+                        customOutput: destination
+                    }
+                    , projectOptions = { path: '.' };
+
+                builder.javascript(fileOptions, projectOptions, function (err) {
+                    if(err) {
+                        grunt.log.err(err);
+                    }
+                });
+            }
+
+            f.src.filter(nonPartials)
+                .filter(function (filepath) {
+                    // Warn on and remove invalid source files (if nonull was set).
+                    return grunt.file.exists(filepath);
+                })
+                .map(function (filepath) {
+                    console.error(filepath)
+                    if (filepath.match(/\.(kit|html)$/)) {
+                        compileKitFile(filepath);
+                    } else if (filepath.match(/\.js$/)) {
+                        compileJsFile(filepath);
+                    } else {
+                        console.warn("No handler for filetype. Unsure what to do with this file: " + filepath);
+                    }
+                });
+        });
     });
-  });
 };
 
