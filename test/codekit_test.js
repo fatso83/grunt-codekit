68f7ae9dfe4097977177fed4fccb8ab2c3e41e34
Carl-Erik Kopseng <carlerik@gmail.com>
2014-07-08 09:50:43 +0200

    Same newline output as CodeKit - Updated to node-kit version 0.6 - Fixed the expected output
    fixes issue #2  and closes issue #7

--- test/codekit_test.js
+++ test/codekit_test.js
@@ -11,15 +11,15 @@ describe('Basic functionality tests', function () {
 
   it('should do basic basic parsing', function() {
 
-    var actual = read('.tmp/test01_result.html');
-    var expected = read('test/expected/test01_expected.html');
+    var actual = read('.tmp/test01_result.html').toString();
+    var expected = read('test/expected/test01_expected.html').toString();
     actual.should.eql(expected);
   });
 
   it('should do variable embedding', function() {
 
-    var actual = read('.tmp/test02_result.html');
-    var expected = read('test/expected/test02_expected.html');
+    var actual = read('.tmp/test02_result.html').toString();
+    var expected = read('test/expected/test02_expected.html').toString();
     actual.should.eql(expected);
   });
 
