e706a8b6a271495ece6fa2a8981eecfeb3b209b5
Scott Roberson <sroberson@gmail.com>
2014-07-24 15:02:39 -0400

    put defaults back in to Gruntfile.js

--- Gruntfile.js
+++ Gruntfile.js
@@ -33,7 +33,7 @@ module.exports = function(grunt) {
 
         // Configuration to be run (and then tested).
         codekit: {
-            main: {
+            defaults: {
                 options: {},
 
                 files: {
